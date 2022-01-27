be5f9544679b09f13aca37c27ea0af08620914fc
Andi-M96 <83184180+Andi-M96@users.noreply.github.com>
2022-01-27 11:43:15 +0000

    oauth

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -55,3 +55,30 @@ class MessagesController extends AbstractController
         return new Response();
     }
 }
+// echo password_hash("andi", PASSWORD_DEFAULT);
+
+// $password = "ande";
+// $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+
+
+$passwordHash = '$2a$04$08KDO94wJ.T5FvBaao.qXO1H1dI1W3MuqPRQWLrnYOBDUMz6imGZW';
+
+// if (password_verify($password, $passwordHash)) {
+//     echo  " passwords match!";
+// } else {
+//     echo "passwords dont match";
+// }
+
+
+
+if ((isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER'] == "onde")) &&
+    (isset($_SERVER['PHP_AUTH_PW']) && (password_verify($_SERVER['PHP_AUTH_PW'], $passwordHash)))
+) {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+} else {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
+    exit;
+}
